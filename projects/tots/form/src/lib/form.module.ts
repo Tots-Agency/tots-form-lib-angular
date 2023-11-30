deff2219df991ebc80cf0bba649d817e1b99cb8a
Álvaro Enuel Serein <alvaro@tots.agency>
2023-11-30 15:46:43 -0300

    removed mat form field provider

--- projects/tots/form/src/lib/form.module.ts
+++ projects/tots/form/src/lib/form.module.ts
@@ -6,7 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 /** Angular Material */
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
